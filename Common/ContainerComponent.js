bb35bdc62c6633952762fa41e69095f1901174b5
TERVER AOSU <terveraosu@gmail.com>
2020-04-19 17:31:41 +0100

    initial commit

---
+++ Common/ContainerComponent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const ContainerComponent = () => {
+  return (
+    <View>
+      <Text>This is the container component</Text>
+    </View>
+  )
+}
+
+export default ContainerComponent
+
+const styles = StyleSheet.create({})
---
+++ Screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>This is home component</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
